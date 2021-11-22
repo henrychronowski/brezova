8e9a7cc14f1b10e2fc957e5ec71628f36192169c
Ethan Heil <eheil20@gmail.com>
2021-11-22 13:35:09 -0500

    Adding run transition

--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_DemoMode1_Animation/a3_DemoMode1_Animation-idle-update.c
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/a3_DemoMode1_Animation/a3_DemoMode1_Animation-idle-update.c
@@ -511,12 +511,19 @@ void a3animation_update(a3_DemoState* demoState, a3_DemoMode1_Animation* demoMod
 	// process input
 	
 	a3boolean jumping = a3keyboardGetState(demoState->keyboard, a3key_space);
+	a3real runThreshold = 6.0f;
+	a3real walkThreshold = 2.0f;
+	a3real yPos = demoMode->obj_skeleton_ctrl->position.y;
 
 	if(jumping)
 		a3clipControllerBranchTransition(demoMode->clipCtrlA, demoMode->clipCtrlA->clip, &demoMode->clipCtrlA->clipPool->clip[9], (a3real)jumping, 0.0f);
+	else if(yPos < runThreshold || yPos > -runThreshold)
+	{
+		a3clipControllerBranchTransition(demoMode->clipCtrlA, &demoMode->clipCtrlA->clipPool->clip[8], &demoMode->clipCtrlA->clipPool->clip[10], yPos, 1.0f);
+	}
 	else
 	{
-		a3clipControllerBranchTransition(demoMode->clipCtrlA, &demoMode->clipCtrlA->clipPool->clip[8], &demoMode->clipCtrlA->clipPool->clip[10], demoMode->obj_skeleton_ctrl->position.y, 1.0f);
+		a3clipControllerBranchTransition(demoMode->clipCtrlA, &demoMode->clipCtrlA->clipPool->clip[10], &demoMode->clipCtrlA->clipPool->clip[11], yPos, 5.0f);
 	}
 
 	//a3clipControllerBranchTransition(demoMode->clipCtrlA, &demoMode->clipCtrlA->clipPool->clip[8], &demoMode->clipCtrlA->clipPool->clip[10], demoMode->obj_skeleton_ctrl->position.y, 1.0f);
